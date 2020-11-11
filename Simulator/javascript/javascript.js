b62957688e8cac31e93926e9bd69455371d8298f
Chirag Kava <chiragkava11@gmail.com>
2020-11-12 00:07:50 +0530

    Added new Textarea API

--- Simulator/javascript/javascript.js
+++ Simulator/javascript/javascript.js
@@ -1,45 +1,81 @@
- function updateOutput(){
-                $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" +
-                    $("#htmlPanel").val() + "</body></html>");
+var heditor = ace.edit("htmleditor");
+            heditor.setTheme("ace/theme/monokai");
+            heditor.getSession().setMode("ace/mode/javascript");
+            heditor.session.setOption("useWorker", false);
+            heditor.setFontSize("14px");
+            heditor.resize();
+            heditor.setShowPrintMargin(false);
+            heditor.getSession().setUseWrapMode(true);
+            
+           
+           
+
+            
+            var ceditor = ace.edit("csseditor");
+            ceditor.setTheme("ace/theme/monokai");
+            ceditor.getSession().setMode("ace/mode/javascript");
+            ceditor.session.setOption("useWorker", false);
+            ceditor.setFontSize("14px");
+            ceditor.setShowPrintMargin(false);
+            ceditor.getSession().setUseWrapMode(true);
+            ceditor.getSession().setMode("ace/mode/css");
+            
+        
+            var jeditor = ace.edit("javascripteditor");
+            jeditor.setTheme("ace/theme/monokai");
+            jeditor.setShowPrintMargin(false);
+            jeditor.getSession().setMode("ace/mode/javascript");
+            jeditor.session.setOption("useWorker", false);
+            jeditor.setFontSize("14px");
+            jeditor.getSession().setUseWrapMode(true);
+            
+        
+            function updateOutput(){
+                $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + ceditor.getSession().getValue() + "</style></head><body>" +
+                    heditor.getSession().getValue() + "</body></html>");
                
                
-                document.getElementById("outputPanel").contentWindow. eval($("#javascriptPanel").val());
+                document.getElementById("outputPanel").contentWindow. eval(jeditor.getSession().getValue());
+                heditor.resize();
+                ceditor.resize();
+                jeditor.resize();
                
            }
            
             $(".toggleButton").hover(function(){
                $(this).addClass("highlightedButton") ;
-           },function(){
+            },function(){
                $(this).removeClass("highlightedButton");
-           })
-           var pan;
-           $(".toggleButton").click(function(){
+            })
+           
+            $(".toggleButton").click(function(){
                $(this).toggleClass("active");
                
-               var panelId=$(this).attr("id")+"Panel";
+               var panelId=$(this).attr("id")+"editor";
+               // alert(panelId);
                $("#"+panelId).toggleClass("hidden");
                var num = 4-$(".hidden").length;
-               $(".panel").width(($(window).width() / num)-8);
+               $(".editor").width(($(window).width() / num)-1);
+                
              //  $("#"+panelId).toggleClass("whiteback");
+                heditor.resize();
+                ceditor.resize();
+                jeditor.resize();
                
-           })
+            })
         
-           $(".panel").height($(window).height()-$(".navbar").height()-10);
+            $(".editor").height($(window).height()-$(".navbar").height()-10);
             
-           $(".panel").width(($(window).width() / 2)-8);
-           updateOutput();
-           $("textarea").hover(function(){
-                pan=$(this).attr("id");
-                $("#"+pan).addClass("whiteback");
-              // alert(pan);
-               
-           }, function(){
-                $("#"+pan).removeClass("whiteback");
-           });
-           
+            $(".editor").width(($(window).width() / 2)-1);
+                updateOutput();
+                
+          
         
-           $("textarea").on("change keyup paste",function(){
-            
-              updateOutput();
+            $("textarea").on("change keyup paste",function(){
+                
+                updateOutput();
                
-           });
+            });
+            
+        
+            
